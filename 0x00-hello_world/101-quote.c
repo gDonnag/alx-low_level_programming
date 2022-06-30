25101204e08baf7fa5892c4246059f0108fb7fc2
Donna Govender <gracem.naidoo@gmail.com>
2022-06-30 10:11:56 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+and that piece of art is useful" - Dora Korpar, 2015-10-19#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.

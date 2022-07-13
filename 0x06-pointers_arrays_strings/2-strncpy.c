3aec1d14805b780229801db8d5fdb5db47d11b15
Donna Govender <gracem.naidoo@gmail.com>
2022-07-13 11:00:13 -0700

    Task02

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string up to n
- * @dest: Destination of the pointer to the string
- * @src: Source of the pointer to the string to copy
- * @n: Number of bytes to copy.
- * Return: Pointer to the destination string.
- */
+* _strncpy - copies a string up to n
+* @dest: Destination of the pointer to the string
+* @src: Source of the pointer to the string to copy
+* @n: Number of bytes to copy
+* Return: Pointer to the destination string
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;

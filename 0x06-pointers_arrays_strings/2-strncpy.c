d263b3df124c573c8220c3b38282da2929666028
Donna Govender <gracem.naidoo@gmail.com>
2022-07-13 10:47:20 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,14 +7,13 @@
  * @n: Number of bytes to copy.
  * Return: Pointer to the destination string.
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
-		dest[i]	= src[i];
-	while (i < n)
-		dest[i++] = '\0';
-	return (dest);
+for (i = 0; src[i] != '\0' && i < n; i++)
+dest[i]	= src[i];
+while (i < n)
+dest[i++] = '\0';
+return (dest);
 }

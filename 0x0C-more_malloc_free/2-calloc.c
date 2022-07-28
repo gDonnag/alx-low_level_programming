96b40f06f712384bdd0b43bd019cfe57a316b027
Donna Govender <106972948+gDonnag@users.noreply.github.com>
2022-07-28 20:30:56 +0200

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,25 +1,21 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
-
 /**
- * _calloc - print 0s into 2D grid
- * @nmemb: array num
- * @size: size of ea array element
- * Return: 0
- */
+  * _calloc - allocates memory for an initialized buffer
+  * @nmemb: number of memory bytes
+  * @size: number of data type bits
+  * Return: return pointer to initialized buffer
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *arr;
-unsigned int i;
+	int *p;
+	unsigned int i;
 
-if (nmemb == 0 || size == 0)
-return (NULL);
-
-arr = malloc(nmemb * size);
-if (arr == NULL)
-return (NULL);
-
-for (i = 0; i < (nmemb * size); i++)
-arr[i] = 0;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb ; i++)
+		p[i] = 0;
+	return (p);
 }
